ff97cb8bc7fbf3cea3866e777c9b3299085e6ba1
nyanko3 <nemusugiru2@outlook.com>
2024-10-18 18:38:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 from cache import cache
 
 
-max_api_wait_time = 3
+max_api_wait_time = 6
 max_time = 10
 apis = [r"https://invidious.jing.rocks/",r"https://invidious.nerdvpn.de/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
