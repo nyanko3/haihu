31dfb1fc1abe0ec0d019031d9f333d986943bc98
nyanko3 <nemusugiru2@outlook.com>
2024-10-18 13:26:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cache import cache
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://inv.nadeko.net/",r"https://invidious.qwik.space/",r"https://y.com.sb/",r"https://youtube.privacyplz.org/",r"https://yt.yoc.ovh/",r"https://invidious.jing.rocks/",r"https://invidious.nerdvpn.de/"]
+apis = [r"https://inv.nadeko.net/",r"https://invidious.0011.lt/",r"https://invidious.ethibox.fr/",r"https://invidious.flokinet.to/",r"https://y.com.sb/",r"https://youtube.privacyplz.org/",r"https://yt.yoc.ovh/",r"https://invidious.jing.rocks/",r"https://invidious.nerdvpn.de/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
