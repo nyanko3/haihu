137f2612de3b3868be7145b176811b8b5d36b7bb
眠いなぁ <nemusugiru2@outlook.com>
2024-11-11 16:07:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ def apicommentsrequest(url):
 video_apis = [
     r"https://invidious.jing.rocks/",
     r"https://invidious.nerdvpn.de/",
-   r"https://script.google.com/macros/s/AKfycbx_TAgfhSTiQXB_jFgj-WeAZsHGnOhiPz0yD4q6BqcST524C-rTLLCVIDK0D3Zbn2OC"
+   r"https://script.google.com/macros/s/1bairVgWtkz1B0pYz1cpntzPbBThsoJsf6i4RE2c02z_6sHWoaiMPbBdB/exec?videoId="
 ]
 
 # get_data 関数の変更
