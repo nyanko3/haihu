8aa8ec73624034a31a31a0b8993bbd959c1cefed
眠いなぁ <nemusugiru2@outlook.com>
2024-12-22 19:26:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ header = {
 
 class InvidiousAPI:
     def __init__(self):
-        self.videos = ast.literal_eval(requests.get('https://raw.githubusercontent.com/nyanko3/invidious/refs/heads/main/instances.txt', headers=header, timeout=(1.0, 0.5)).text)
+        self.videos = ast.literal_eval(requests.get('https://gitlab.com/nyanko_a1/invidious/-/raw/main/instances.txt?ref_type=heads', headers=header, timeout=(1.0, 0.5)).text)
         
         self.channels = []
         self.comments = []
